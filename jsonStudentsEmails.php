e47358e2a1305acd344873e742210577f33d509b
Abbas196 <abbasraza196@gmail.com>
2022-11-18 15:34:07 -0600

    pushing student validaton php classes

--- jsonStudentsEmails.php
+++ jsonStudentsEmails.php
@@ -1,7 +1,8 @@
 <?php
 require_once "pdo.php";
+session_start();
 header('Content-Type: application/json; charset=utf-8');
-$stmt = $pdo->query("SELECT email from students");
+$stmt = $pdo->query("SELECT * from students");
 $rows = array();
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $rows[] = $row;
--- studentValidate.php
+++ studentValidate.php
@@ -1,5 +1,6 @@
 <?php
 require_once "pdo.php";
+session_start();
 ?>
 
 
@@ -11,18 +12,21 @@ require_once "pdo.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/studentLogin.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <title>Student Login</title>
 </head>
 
 <body>
     <p class="studentLogin">Student Login</p>
-    <form method="POST" id="studentForm">
+    <form method="POST" id="studentForm" action="quiz.php">
         <label for="studentEmail">Email ID</label>
         <input type="text" id="studentEmail" name="studentEmail" class="loginInput"><br>
         <label for="password">Password</label>
         <input type="text" id="password" name="password" class="loginInput"><br>
-        <a href="passwordReset.php" style="float:inline-start">forgot password ?</a>
+        <a href="passwordReset.php" style="float:inline-start">forgot password ?</a> <br><br>
+        <input type="submit" value="submit" id="submitButton" style="width: 130px;background-color:cadetblue;border-radius:10px">
     </form>
+    <script src="./JS_files/studentValidate.js"></script>
 </body>
 
 </html>
