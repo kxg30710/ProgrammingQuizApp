1751693ca8c91de9dc6be0ab78356cfd28e62889
Abbas196 <abbasraza196@gmail.com>
2022-11-29 17:49:32 -0600

    code pushed

--- Course_select.php
+++ Course_select.php
@@ -11,12 +11,16 @@ $_SESSION['password'] = $_POST['password'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/courseSelect.css">
     <title>Course List</title>
 </head>
 
 <body>
-    <h1 id="heading">List of the courses assigned</h1>
+    <div class="navbar navbar-dark bg-primary">
+        <h1 id="heading">List of the courses assigned</h1>
+    </div>
+
     <form action="exam.php" method="POST">
         <?php
         $querry = "select * from student_assignment where stud_id = :id";
